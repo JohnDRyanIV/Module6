8ab86d442a8d992bc90c0b04c7d72caea14d0609
Diamondfiend <jdryan30@yahoo.com>
2020-02-26 15:16:37 -0600

    first two tests pass, implementation made both of them pass

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -1,10 +1,12 @@
-def score_input(test_name, test_score = 0, invalid_message = "Invalid test score, try again"):
+def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again"):
     """
     :param test_name: represents name of the test
     :param test_score: represents score on test, valid between 0 and 100, inclusive
     :param invalid_message: represents message shown to user upon invalid input
     :return message: represents message being printed
     """
+    message = test_name + ": " + str(test_score)
+    return message
     pass
 
 
