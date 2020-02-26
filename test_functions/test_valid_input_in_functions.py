dfc4ef731e04de7a6111138acfabd378d751f6f8
Diamondfiend <jdryan30@yahoo.com>
2020-02-26 15:26:49 -0600

    modified style in test files

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -13,12 +13,15 @@ class MyTestCase(unittest.TestCase):
     def test_score_input_test_score_below_range(self):
         self.assertEqual(h.score_input("Comp", -27), "Comp: 27")
         # Will input 27 as test score after validation
+
     def test_score_input_test_score_above_range(self):
         self.assertEqual(h.score_input("Comp", 178), "Comp: 78")
         # Will enter 78 as test score after validation
+
     def test_score_input_test_non_numeric(self):
         self.assertEqual(h.score_input("Comp", "!%$#@@#"), "Comp: 91")
         # Will enter 91 as test score after validation
+
     def test_score_input_invalid_message(self):
         self.assertEqual(h.score_input("Comp", 150, "Incorrect input"), "Comp: 83")
         # Will enter 83 as test score after validation
