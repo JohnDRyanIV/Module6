38771e491091abea33dc4556196db18a5f61b1d4
Diamondfiend <jdryan30@yahoo.com>
2020-02-26 16:43:23 -0600

    all tests pass, area & perimeter dependent on modulus

--- source_files/inner_functions_assignment.py
+++ source_files/inner_functions_assignment.py
@@ -1,13 +1,21 @@
 def measurements(x):
+    """
+    :param x: list containing measurements of shape being passed
+    :return message: string detailing measurements being returned
+    """
     def area(y):
-        return y[0] * y[1]
+        """
+        :param y: list containing measurements of shape being passed
+        :return: area measurement of shape
+        """
+        return y[len(y) % 3 - 1] * y[0]
 
     def perimeter(z):
-        return 2*z[0] + 2*z[1]
+        """
+        :param z: listing containing measurements of shape being passed
+        :return: perimeter measurement of shape
+        """
+        return 2 * z[len(z) % 3 - 1] + 2 * z[0]
 
     message = "Perimeter = " + str(perimeter(x)) + " Area = " + str(area(x))
-    print(message)
     return message
-    pass
-
-
