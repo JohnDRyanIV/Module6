2f11589d5d5ae0845caaf44f4db974897141d58d
Diamondfiend <jdryan30@yahoo.com>
2020-02-26 15:26:15 -0600

    modified test files to fit criteria better, first 3 tests pass

--- test_functions/test_valid_input_in_functions.py
+++ test_functions/test_valid_input_in_functions.py
@@ -11,17 +11,17 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(h.score_input("Comp", 90), "Comp: 90")
 
     def test_score_input_test_score_below_range(self):
-        self.assertEqual(h.score_input("Comp", -50), "Invalid test score, try again")
-
+        self.assertEqual(h.score_input("Comp", -27), "Comp: 27")
+        # Will input 27 as test score after validation
     def test_score_input_test_score_above_range(self):
-        self.assertEqual(h.score_input("Comp", 150), "Invalid test score, try again")
-
+        self.assertEqual(h.score_input("Comp", 178), "Comp: 78")
+        # Will enter 78 as test score after validation
     def test_score_input_test_non_numeric(self):
-        self.assertEqual(h.score_input("Comp", "!%$#@@#"), "Invalid test score, try again")
-
+        self.assertEqual(h.score_input("Comp", "!%$#@@#"), "Comp: 91")
+        # Will enter 91 as test score after validation
     def test_score_input_invalid_message(self):
-        self.assertEqual(h.score_input("Comp", 150, "Incorrect input"), "Incorrect input")
-        self.assertEqual(h.score_input("Comp", 50, "Incorrect input"), "Comp: 50")
+        self.assertEqual(h.score_input("Comp", 150, "Incorrect input"), "Comp: 83")
+        # Will enter 83 as test score after validation
 
 
 if __name__ == '__main__':
