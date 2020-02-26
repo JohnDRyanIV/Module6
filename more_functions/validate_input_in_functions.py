08a393b9293fd1878b2cc9d874219045d88d1a16
Diamondfiend <jdryan30@yahoo.com>
2020-02-26 16:14:57 -0600

    revamp of input validation, all tests pass

--- more_functions/validate_input_in_functions.py
+++ more_functions/validate_input_in_functions.py
@@ -1,14 +1,22 @@
-def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again"):
+def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again!"):
     """
     :param test_name: represents name of the test
     :param test_score: represents score on test, valid between 0 and 100, inclusive
     :param invalid_message: represents message shown to user upon invalid input
     :return message: represents message being printed
     """
+    while True:
+        try:
+            if 0 <= test_score <= 100:
+                break
+        except TypeError as err:
+            pass
+        try:
+            print(invalid_message + " (" + str(test_score) + ")")
+            test_score = int(input("Enter the test score: "))
+        except ValueError as err:
+            pass
 
-    while test_score < 0 or test_score > 100:
-        print(str(invalid_message + " (" + str(test_score) + ")"))
-        test_score = int(input("Enter the test score: "))
     message = test_name + ": " + str(test_score)
     return message
 
