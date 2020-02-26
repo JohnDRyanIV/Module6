09bd59924301a68668bc4d1b4665922b7bb2b5ce
Diamondfiend <jdryan30@yahoo.com>
2020-02-26 14:41:03 -0600

    implemented multiply_string, all tests pass

--- more_functions/string_functions.py
+++ more_functions/string_functions.py
@@ -1,3 +1,29 @@
-def multiply_string():
-    pass
+# Program: string_functions
+# Author: John Ryan
+# Last modified: 2/26/2020
 
+# This program takes a string and number (n) input by the user and outputs that string
+# n number of times
+
+
+def multiply_string(m, n):
+    """
+    returns string m repeating n times
+    :param m: represents message to be repeated
+    :param n: represents amount of times message will repeat
+    """
+    r = ""
+    for i in range(0, n):
+        r = r + m
+    return r
+
+
+if __name__ == '__main__':
+    try:
+        message = str(input("Enter a message: "))
+        repeat = int(input("Enter the number of times you want that message to repeat: "))
+    except ValueError as err:
+        print("Invalid input. Run again.")
+    else:
+        message = multiply_string(message, repeat)
+    print(message)
--- test_functions/test_string_functions.py
+++ test_functions/test_string_functions.py
@@ -4,8 +4,8 @@ from more_functions import string_functions
 
 class MyTestCase(unittest.TestCase):
     def test_multiple_string(self):
-        self.assertEqual(string_functions.multiply_string("Ayah, 3"), "AyahAyahAyah")
-        self.assertEqual(string_functions.multiply_string("John, 4"), "JohnJohnJohnJohn")
+        self.assertEqual(string_functions.multiply_string("Ayah", 3), "AyahAyahAyah")
+        self.assertEqual(string_functions.multiply_string("John", 4), "JohnJohnJohnJohn")
 
 
 if __name__ == '__main__':
